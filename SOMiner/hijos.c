1ee386934b3ebb9efa2c88c4398b5c820ea85187
ElLapiz <56371415+ElLapiz@users.noreply.github.com>
2019-12-04 21:36:06 -0600

    Added files
    First commit for this rep

---
+++ SOMiner/hijos.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "tareas.c"
+
+#define ERROR -1
+
+pid_t childPid;
+
+int toAscii(char **param){
+    return (int)param;
+}
+
+void asignaTrabajo(int numero) {
+    switch (numero) {
+        case 99:   //c
+            collectCpuData(numero);
+            break;
+        case 100:  //d
+            collectDiskData(numero);
+            break;
+        case 109:  //m
+            collectMemData(numero);
+            break;
+        case 110: //n
+            collectNetData(numero);
+            break;
+        deafult:
+            break;
+    }
+}
+
+int creaHijos(pid_t childPid, int argc, char **argv) {
+    int i;
+
+    for (i = 1; i <= sizeof(argc); i++) {
+        switch (childPid = fork()) {
+            int choice;
+            case -1:
+                return ERROR;
+            case 0:
+                choice = toAscii(argv[i][0]);
+                asignaTrabajo(choice);
+                _exit(0);
+            default:
+                sleep(1);
+                break;
+        }
+
+    }
+}
